62a60d70649a02924d1a07fb512c01d89a0fc2f9
Ivan Psarev <psarew.ivan@yandex.ru>
2019-02-03 22:46:30 +0300

    Начал стилизовать. Корректура разметки

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//var searchInput = document.querySelector('#search');
+//var searchButton = document.querySelector('.search-button');
+//
+//searchButton.classList.add('button-hide');
+//
+//searchInput.addEventListener('focus', classRemove());
+////searchInput.removeEventListener('focus', classRemove);
+//
+//function classRemove() {
+//  searchButton.classList.remove('button-hide');
+//}
