a0754e55b0a7b7476d038de41484dedaf4171d5c
Ivan Psarev <psarew.ivan@yandex.ru>
2019-02-04 23:07:15 +0300

    Делаю шапку. Опять проблемы с выпадающим меню.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-//var searchInput = document.querySelector('#search');
-//var searchButton = document.querySelector('.search-button');
-//
-//searchButton.classList.add('button-hide');
-//
-//searchInput.addEventListener('focus', classRemove());
-////searchInput.removeEventListener('focus', classRemove);
-//
-//function classRemove() {
-//  searchButton.classList.remove('button-hide');
-//}
+//сделать динамический показ имени пользателя
+
+var loginButton = document.querySelector('.login-button');
+var userName = document.querySelector('.user-login');
+var logoutButton = document.querySelector('.logout-button');
+
+loginButton.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  userName.classList.remove('display-none');
+  logoutButton.classList.remove('display-none');
+  loginButton.classList.add('display-none');
+});
+
+logoutButton.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  userName.classList.add('display-none');
+  logoutButton.classList.add('display-none');
+  loginButton.classList.remove('display-none');
+});
