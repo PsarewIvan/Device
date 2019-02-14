22979e56480f3e64a64ceb6a1d950c4b5055b25c
Ivan Psarev <psarew.ivan@yandex.ru>
2019-02-14 22:34:53 +0300

    добавил модалку

--- js/script.js
+++ js/script.js
@@ -17,3 +17,80 @@ logoutButton.addEventListener('click', function(evt) {
   logoutButton.classList.add('display-none');
   loginButton.classList.remove('display-none');
 });
+
+//оживляем модалку
+var modalButton = document.querySelector('.write-us-button');
+var popup = document.querySelector('.modal-contact-us');
+var modalCloseButton = popup.querySelector('.close-modal');
+var inputName = popup.querySelector('#user-name');
+var inputEmail = popup.querySelector('#user-email');
+var inputMessage =popup.querySelector('#user-message');
+var modalForm = popup.querySelector('.modal-form');
+
+var isStorage = true;
+var storageName = '';
+var storageEmail = '';
+
+try {
+  storageName = localStorage.getItem('login');
+  storageEmail = localStorage.getItem('email');
+} catch(err) {
+  isStorage = false;
+}
+
+modalButton.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  popup.classList.remove('display-none');
+  popup.classList.add('modal-show');
+  if (storageName && storageEmail) {
+    inputName.value = storageName;
+    inputEmail.value = storageEmail;
+    inputMessage.focus();
+  } else if (storageEmail) {
+    inputEmail.value = storageEmail;
+    inputName.focus();
+  } else if (storageName) {
+    inputName.value = storageName;
+    inputEmail.focus();
+  } else {
+    inputMessage.focus();
+  }
+});
+
+modalCloseButton.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  popup.classList.add('display-none');
+  popup.classList.remove('modal-show');
+  popup.classList.remove('modal-error');
+});
+
+modalForm.addEventListener('submit', function(evt) {
+  if (!inputEmail.value || !inputMessage.value || !inputMessage.value) {
+    evt.preventDefault();
+    popup.classList.remove('modal-error');
+    void popup.offsetWidth;
+    popup.classList.add('modal-error');
+  } else {
+    if (isStorage) {
+      localStorage.setItem('login', inputName.value);
+      localStorage.setItem('email', inputEmail.value);
+    }
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
